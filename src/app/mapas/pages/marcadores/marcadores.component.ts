0463f3d48160145ba8d22b4521555d4069181893
German Arevalo J <germanarevalojerez@gmail.com>
2021-10-12 17:53:02 -0500

    init project

---
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -0,0 +1,42 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-marcadores',
+  templateUrl: './marcadores.component.html',
+  styles: [
+    `
+      .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+    `,
+  ],
+})
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [9.138608503780008, 48.81540935503443];
+
+  constructor() {}
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    const marker = new mapboxgl.Marker()
+      .setLngLat(this.center)
+      .addTo(this.mapa);
+  }
+}
